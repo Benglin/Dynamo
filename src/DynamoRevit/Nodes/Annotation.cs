57143880473ce6c030af9921ecf2b5d986d55478
Ben Goh <benglin@outlook.com>
2014-01-20 11:47:37 +0800

    Provided first cut implementation for "ModelText.Migrate_0630_to_0700"

--- src/DynamoRevit/Nodes/Annotation.cs
+++ src/DynamoRevit/Nodes/Annotation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Xml;
 using Autodesk.Revit.DB;
 using Dynamo.Models;
 using Dynamo.Revit;
@@ -102,8 +103,120 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            // TODO(Ben): Implement this.
-            throw new NotImplementedException();
+            #region Migration Setup Steps
+
+            NodeMigrationData migratedData = new NodeMigrationData(data.Document);
+
+            // Legacy "ModelText" node takes in the following 6 inputs:
+            // 
+            //      0 - text (string)
+            //      1 - position (XYZ)
+            //      2 - normal (XYZ)
+            //      3 - up (XYZ)
+            //      4 - depth (double)
+            //      5 - text type name (string)
+            // 
+            // The new "DSModelText.ByTextSketchPlaneAndPosition" node takes in
+            // the following inputs:
+            // 
+            //      0 - text (string)
+            //      1 - sketchPlane (DSSketchPlane)
+            //      2 - xCoordinateInPlane (double)
+            //      3 - yCoordinateInPlane (double)
+            //      4 - textDepth (double)
+            //      5 - modelTextType (DSModelTextType)
+            // 
+            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
+            string oldNodeId = MigrationManager.GetGuidFromXmlElement(oldNode);
+
+            #endregion
+
+            #region Create New Nodes...
+
+            // Create the "DSModelText.ByTextSketchPlaneAndPosition" node itself.
+            XmlElement dsModelText = MigrationManager.CreateFunctionNode(
+                data.Document, "DSRevitNodes.dll",
+                "DSModelText.ByTextSketchPlaneAndPosition",
+                "DSModelText.ByTextSketchPlaneAndPosition@" +
+                "string,DSSketchPlane,double,double,double,DSModelTextType");
+
+            migratedData.AppendNode(dsModelText);
+            string dsModelTextId = MigrationManager.GetGuidFromXmlElement(dsModelText);
+
+            // Create a "Plane.ByOriginNormal" that takes a "Point" (origin) and 
+            // a "Vector" (normal). This new node will convert both the "position" 
+            // and "normal" to a "Plane".
+            XmlElement plane = MigrationManager.CreateFunctionNode(
+                data.Document, "ProtoGeometry.dll", "Plane.ByOriginNormal",
+                "Plane.ByOriginNormal@Point,Vector");
+
+            migratedData.AppendNode(plane);
+            string planeId = MigrationManager.GetGuidFromXmlElement(plane);
+
+            // Create a "DSSketchPlane.ByPlane" node which converts a "Plane" 
+            // into a "DSSketchPlane".
+            XmlElement dsSketchPlane = MigrationManager.CreateFunctionNode(
+                data.Document, "DSRevitNodes.dll",
+                "DSSketchPlane.ByPlane", "DSSketchPlane.ByPlane@Plane");
+
+            migratedData.AppendNode(dsSketchPlane);
+            string dsSketchPlaneId = MigrationManager.GetGuidFromXmlElement(dsSketchPlane);
+
+            // Create a "DSModelTextType.ByName" node that converts a "string"
+            // into "DSModelTextType" node.
+            XmlElement dsModelTextType = MigrationManager.CreateFunctionNode(
+                data.Document, "DSRevitNodes.dll",
+                "DSModelTextType.ByName", "DSModelTextType.ByName@string");
+
+            migratedData.AppendNode(dsModelTextType);
+            string dsModelTextTypeId = MigrationManager.GetGuidFromXmlElement(dsModelTextType);
+
+            #endregion
+
+            #region Move Connectors Onto the New Nodes
+
+            // Move connector for "text" over to the new node.
+            PortId oldInPort = new PortId(oldNodeId, 0, PortType.INPUT);
+            PortId newInPort = new PortId(dsModelTextId, 0, PortType.INPUT);
+            XmlElement connector = data.FindConnectors(oldInPort).ElementAt(0);
+            data.ReconnectToPort(connector, newInPort);
+
+            // Move connector for "position" over to "Plane" node.
+            oldInPort = new PortId(oldNodeId, 1, PortType.INPUT);
+            newInPort = new PortId(planeId, 0, PortType.INPUT);
+            connector = data.FindConnectors(oldInPort).ElementAt(0);
+            data.ReconnectToPort(connector, newInPort);
+
+            // Move connector for "normal" over to "Plane" node.
+            oldInPort = new PortId(oldNodeId, 2, PortType.INPUT);
+            newInPort = new PortId(planeId, 1, PortType.INPUT);
+            connector = data.FindConnectors(oldInPort).ElementAt(0);
+            data.ReconnectToPort(connector, newInPort);
+
+            // Connect from "Plane" to "DSSketchPlane".
+            data.CreateConnector(plane, 0, dsSketchPlane, 0);
+
+            // Connect from "DSSketchPlane" to the new node.
+            data.CreateConnector(dsSketchPlane, 0, dsModelText, 1);
+
+            // Move connector for "depth" over to the new node.
+            oldInPort = new PortId(oldNodeId, 4, PortType.INPUT);
+            newInPort = new PortId(dsModelTextId, 4, PortType.INPUT);
+            connector = data.FindConnectors(oldInPort).ElementAt(0);
+            data.ReconnectToPort(connector, newInPort);
+
+            // Move connector for "text type name" over to "DSModelTextType" node.
+            oldInPort = new PortId(oldNodeId, 5, PortType.INPUT);
+            newInPort = new PortId(dsModelTextTypeId, 0, PortType.INPUT);
+            connector = data.FindConnectors(oldInPort).ElementAt(0);
+            data.ReconnectToPort(connector, newInPort);
+
+            // Connect from "DSModelTextType" to the new node.
+            data.CreateConnector(dsModelTextType, 0, dsModelText, 5);
+
+            #endregion
+
+            return migratedData;
         }
     }
 }
