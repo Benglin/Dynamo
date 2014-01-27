8811e952eea9a6170169445abb3c7d6ddad11276
ellensi <ellensi.chandra@autodesk.com>
2014-01-27 15:46:09 +0800

    Merged conflict at dynBaseTypes.cs

--- src/DynamoCore/Nodes/dynBaseTypes.cs
+++ src/DynamoCore/Nodes/dynBaseTypes.cs
@@ -790,7 +790,7 @@ namespace Dynamo.Nodes
             {
                 throw e; //TODO: Better error message
             }
-
+        
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
             return MigrateToDsFunction(data, "DSCoreNodes.dll", "List.SortByKey", "List.SortByKey@var[],var");
