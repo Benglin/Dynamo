2f271135d071a1634dcae0d900345b3050af594d
Zhengzi <flymaple1213@gmail.com>
2014-01-30 13:38:31 +0800

    remove a migariton function named DSReferencePoint.ByPoint in DynamoRevit/Nodes/ReferencePoint.cs, since it doesnot work.

--- src/DynamoRevit/Nodes/ReferencePoint.cs
+++ src/DynamoRevit/Nodes/ReferencePoint.cs
@@ -369,14 +369,6 @@ namespace Dynamo.Nodes
             }
             return Value.NewContainer(r);
         }
-
-        [NodeMigration(from: "0.6.3", to: "0.7.0.0")]
-        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
-        {
-            return MigrateToDsFunction(data, "DSRevitNodes.dll",
-                "DSReferencePoint.ByPoint",
-                "DSReferencePoint.ByPoint@Point");
-        }
     }
 
     [NodeName("Reference Point at Length")]
