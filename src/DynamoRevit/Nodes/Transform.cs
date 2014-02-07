85c8336f4319444a3ca7f52927a82f0a09b9b68d
Zhengzi <flymaple1213@gmail.com>
2014-02-07 14:41:17 +0800

    delete two empty lines in Transform.cs

--- src/DynamoRevit/Nodes/Transform.cs
+++ src/DynamoRevit/Nodes/Transform.cs
@@ -333,9 +333,7 @@ namespace Dynamo.Nodes
             Curve crv = (Curve)((Value.Container)args[0]).Item;
             double parameter = ((Value.Number)args[1]).Item;
 
-
-            Transform tCurve = crv.ComputeDerivatives(parameter, false);
-            
+            Transform tCurve = crv.ComputeDerivatives(parameter, false);           
 
             Transform tF = Transform.Identity;
             tF.Origin = tCurve.Origin;
