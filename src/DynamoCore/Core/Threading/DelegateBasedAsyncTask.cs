d66382e62a99c5a466c2158924ad2baeb32cdc5a
Benglin (Ben) Goh <benglin@outlook.com>
2014-11-12 15:28:31 +0800

    Updated "DelegateBasedAsyncTask" to allow "TaskPriority" overwrite

--- src/DynamoCore/Core/Threading/DelegateBasedAsyncTask.cs
+++ src/DynamoCore/Core/Threading/DelegateBasedAsyncTask.cs
@@ -4,33 +4,40 @@ using System;
 
 namespace Dynamo.Core.Threading
 {
+    internal struct DelegateBasedParams
+    {
+        internal DynamoScheduler DynamoScheduler { get; set; }
+        internal AsyncTask.TaskPriority TaskPriority { get; set; }
+        internal Action ActionToPerform { get; set; }
+    }
+
     /// <summary>
     /// DelegateBasedAsyncTask allows for a delegate or System.Action object 
-    /// to be scheduled for asynchronous execution on the ISchedulerThread. 
+    /// to be scheduled for asynchronous execution on the ISchedulerThread. A 
+    /// DelegateBasedAsyncTask has a default priority of TaskPriority.Normal, 
+    /// which can be overwritten to other priority values through construction.
     /// </summary>
     /// 
     internal class DelegateBasedAsyncTask : AsyncTask
     {
-        private Action actionToPerform;
+        private readonly Action actionToPerform;
+        private readonly TaskPriority assignedPriority = TaskPriority.Normal;
 
         internal override TaskPriority Priority
         {
-            get { return TaskPriority.Normal; }
+            get { return assignedPriority; }
         }
 
         #region Public Class Operational Methods
 
-        internal DelegateBasedAsyncTask(DynamoScheduler scheduler)
-            : base(scheduler)
+        internal DelegateBasedAsyncTask(DelegateBasedParams initParams)
+            : base(initParams.DynamoScheduler)
         {
-        }
+            if (initParams.ActionToPerform == null)
+                throw new ArgumentNullException("initParams.ActionToPerform");
 
-        internal void Initialize(Action action)
-        {
-            if (action == null)
-                throw new ArgumentNullException("action");
-
-            actionToPerform = action;
+            actionToPerform = initParams.ActionToPerform;
+            assignedPriority = initParams.TaskPriority;
         }
 
         #endregion
