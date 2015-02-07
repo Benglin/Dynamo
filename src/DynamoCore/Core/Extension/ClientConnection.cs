d04e64e118aad97ee513bf4249dba73d48909913
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-07 15:57:10 +0800

    Initial submission of "ClientConnection" and "DynamoListener"

---
+++ src/DynamoCore/Core/Extension/ClientConnection.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Dynamo.Core.Extension
+{
+    class ClientConnection
+    {
+        private readonly byte[] buffer;
+        private readonly Socket socket;
+        private readonly DynamoListener listener;
+        private readonly MemoryStream stream;
+
+        internal ClientConnection(DynamoListener listener, Socket socket)
+        {
+            this.socket = socket;
+            this.listener = listener;
+
+            buffer = new byte[4096];
+            stream = new MemoryStream();
+            BeginReceiveData();
+        }
+
+        internal void SendMessage(byte[] data)
+        {
+            var e = new SocketAsyncEventArgs
+            {
+                UserToken = this
+            };
+
+            bool completedAsynchronously = false;
+
+            try
+            {
+                e.SetBuffer(data, 0, data.Length);
+                e.Completed += OnSendMessageCompleted;
+                completedAsynchronously = socket.SendAsync(e);
+            }
+            catch (Exception)
+            {
+            }
+
+            if (!completedAsynchronously)
+                OnSendMessageCompleted(this, e);
+        }
+
+        private void OnSendMessageCompleted(object sender, SocketAsyncEventArgs e)
+        {
+        }
+
+        private void BeginReceiveData()
+        {
+            socket.BeginReceive(buffer, 0, buffer.Length,
+                SocketFlags.None, ReceiveCallback, this);
+        }
+
+        private void RemoveSelfFromClientList()
+        {
+            listener.RemoveFromClientList(this);
+        }
+
+        private void FlushToStream(int bytesToFlush)
+        {
+            if (bytesToFlush <= 0)
+            {
+                var data = Encoding.UTF8.GetString(stream.GetBuffer());
+                stream.Position = 0; // Reset write position.
+            }
+            else
+            {
+                stream.Write(buffer, ((int)stream.Position), bytesToFlush);
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            var clientConnection = ((ClientConnection)ar.AsyncState);
+
+            try
+            {
+                var socket = clientConnection.socket;
+                clientConnection.FlushToStream(socket.EndReceive(ar));
+                clientConnection.BeginReceiveData();
+            }
+            catch (SocketException exception)
+            {
+                // Client disconnected, remove from client list.
+                clientConnection.RemoveSelfFromClientList();
+            }
+        }
+    }
+}
---
+++ src/DynamoCore/Core/Extension/DynamoListener.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Dynamo.Core.Extension
+{
+    class DynamoListener
+    {
+        private enum EventIndex
+        {
+            ReadyForConnection, Shutdown, Count
+        }
+
+        private readonly Thread workerThread;
+        private readonly TcpListener listener;
+        private readonly List<ClientConnection> clients;
+        private readonly ManualResetEvent[] events;
+
+        internal DynamoListener()
+        {
+            events = new[]
+            {
+                new ManualResetEvent(false),
+                new ManualResetEvent(false)
+            };
+
+            clients = new List<ClientConnection>();
+            workerThread = new Thread(ListenerThreadPro)
+            {
+                IsBackground = true
+            };
+
+            IPAddress localHostAddress = null;
+            IPAddress.TryParse("127.0.0.1", out localHostAddress);
+            listener = new TcpListener(localHostAddress, 3002);
+        }
+
+        internal void StartListening()
+        {
+            listener.Start();
+            workerThread.Start(this);
+        }
+
+        internal void StopListening()
+        {
+            events[(int)EventIndex.Shutdown].Set();
+            workerThread.Join();
+
+            // TODO: Clean-up internal.
+        }
+
+        internal void AddToClientList(ClientConnection client)
+        {
+            lock (clients)
+            {
+                clients.Add(client);
+            }
+        }
+
+        internal void RemoveFromClientList(ClientConnection client)
+        {
+            lock (clients)
+            {
+                clients.Remove(client);
+            }
+        }
+
+        internal void Broadcast(byte[] data)
+        {
+            lock (clients)
+            {
+                foreach (var clientConnection in clients)
+                {
+                    clientConnection.SendMessage(data);
+                }
+            }
+        }
+
+        private static void ListenerThreadPro(object state)
+        {
+            var dynamoListener = ((DynamoListener)state);
+            var events = dynamoListener.events;
+            var listener = dynamoListener.listener;
+
+            while (true)
+            {
+                events[(int)EventIndex.ReadyForConnection].Reset();
+                listener.BeginAcceptSocket(AcceptSocketCallback, dynamoListener);
+                var handleIndex = WaitHandle.WaitAny(events);
+                if (handleIndex == ((int)EventIndex.Shutdown))
+                    break;
+            }
+        }
+
+        private static void AcceptSocketCallback(IAsyncResult ar)
+        {
+            var dynamoListener = ((DynamoListener)ar.AsyncState);
+            var socket = dynamoListener.listener.EndAcceptSocket(ar);
+
+            // Allow DynamoListener to wait for next connection.
+            dynamoListener.events[(int)EventIndex.ReadyForConnection].Set();
+            dynamoListener.AddToClientList(new ClientConnection(dynamoListener, socket));
+        }
+    }
+}
