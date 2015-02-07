f42a22e8f1615054e48a07c7798fed2a1e9a0e21
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-07 17:49:11 +0800

    Implemented "ExtensionInfo" and "ExtensionManager" classes

---
+++ src/DynamoCore/Core/Extension/ExtensionManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace Dynamo.Core.Extension
+{
+    class ExtensionInfo
+    {
+        private const string ExtensionModuleName = "ExtensionModule";
+        private const string RemoteExecutableName = "RemoteExecutable";
+
+        internal ExtensionInfo(string filePath)
+        {
+            var document = new XmlDocument();
+            document.Load(filePath);
+
+            var childNodes = document.DocumentElement.ChildNodes;
+            foreach (XmlNode childNode in childNodes)
+            {
+                switch (childNode.Name)
+                {
+                    case ExtensionModuleName:
+                        ExtensionModule = childNode.InnerText;
+                        break;
+                    case RemoteExecutableName:
+                        RemoteExecutable = childNode.InnerText;
+                        break;
+                }
+            }
+        }
+
+        internal string ExtensionModule { get; private set; }
+        internal string RemoteExecutable { get; private set; }
+    }
+
+    class ExtensionManager
+    {
+        internal ExtensionManager()
+        {
+            var exePath = Assembly.GetCallingAssembly().Location;
+            var directory = new DirectoryInfo(Path.GetDirectoryName(exePath));
+            var files = directory.GetFiles("*.dynext");
+
+            foreach (var fileInfo in files)
+            {
+                try
+                {
+                    var extensionInfo = new ExtensionInfo(fileInfo.FullName);
+                    LoadExtension(extensionInfo);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void LoadExtension(ExtensionInfo extensionInfo)
+        {
+        }
+    }
+}
